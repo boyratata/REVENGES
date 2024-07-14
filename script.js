822f8dc46843fbce19c8eba397ade47070d6c69d
boyratata <152520757+boyratata@users.noreply.github.com>
2024-07-14 13:47:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var video = document.createElement('video');
     video.autoplay = true;
     video.loop = true;
-    video.muted = true; // Ensure video is muted
+    video.muted = false;
     video.className = 'video-background';
     var source = document.createElement('source');
     source.src = 'https://github.com/boyratata/REVENGES/raw/main/background.mp4'; // Replace with your video URL
